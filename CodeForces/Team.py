31ac55247524a1086cc4447c0f8f60846260a435
Basil1361 <trainerbasil69@gmail.com>
2025-11-16 19:01:40 +0800

    Update

---
+++ CodeForces/Team.py
@@ -0,0 +1,37 @@
+'''
+One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
+
+This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find the number of problems for which they will write a solution.
+Input
+
+The first input line contains a single integer n (1 ≤ n ≤ 1000) — the number of problems in the contest. Then n lines contain three integers each, each integer is either 0 or 1. If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't sure. The second number shows Vasya's view on the solution, the third number shows Tonya's view. The numbers on the lines are separated by spaces.
+Output
+
+Print a single integer — the number of problems the friends will implement on the contest.
+'''
+while True:
+    try:
+        n = int(input('Enter between 1 and 1000: '))
+        if n < 1 or n > 1000: 
+            print("Try again")
+        else:
+            break  # Exit loop when n is valid
+    except ValueError:
+        print("Try again")
+
+count = 0 
+for i in range(n):
+    while True:
+        try:
+            P, V, T = map(int, input("Enter 1 or 0, with spacing for each value: ").split())
+            if (P == 0 or P == 1) and (V == 0 or V == 1) and (T == 0 or T == 1):
+                if P + V + T >= 2:
+                    count += 1
+                break
+            else:
+                print("Try again")
+        except ValueError:
+            print("Try again")
+
+print(count)
+        
